29d050dbc7594a906a159f3debb2da6740c028d3
duanqi0001 <52988149+duanqi0001@users.noreply.github.com>
2021-07-12 12:32:12 +0800

    reg

--- main.py
+++ main.py
@@ -1,40 +1,4 @@
-from __future__ import print_function
-
-import argparse
-from os.path import exists, join, basename
-from torch.utils.data import DataLoader
-from data import get_training_set, get_validation_set, get_testing_set
-import pandas as pd
-import numpy as np
-from image_folder import make_dataset
-from run import EVSRCNNTrainer, EVSRCNNTester
-# from torch.utils.tensorboard import SummaryWriter
-# writer = SummaryWriter('E:/PycharmProjects/EVSR_3/runs')
-
-# from dataset.data import get_training_set, get_test_set
-
-
-# ===========================================================
-# 训练参数设置
-# ===========================================================
-parser = argparse.ArgumentParser(description='eventSR')
-
-parser.add_argument('--trainOrTest', type=str, default='Test', help='训练 batch size')
-parser.add_argument('--batchSize', type=int, default=5, help='训练 batch size')
-parser.add_argument('--testBatchSize', type=int, default=1, help='测试 batch size')
-parser.add_argument('--nEpochs', type=int, default=100, help='训练 epochs 数')
-parser.add_argument('--lr', type=float, default=0.01, help='学习率. Default=0.01')
-parser.add_argument('--seed', type=int, default=123, help='随机种子. Default=123')
-parser.add_argument('--hrTrainDataStr', type=str, default='./dataset/train/LR_2')
-parser.add_argument('--lrTrainDataStr', type=str, default='./dataset/train/LR_1')
-parser.add_argument('--epochModelSaveStr', type=str, default='./result/epoch_%d_model')
-parser.add_argument('--hrValidationDataStr', type=str, default='./dataset/validation/LR_2')
-parser.add_argument('--lrValidationDataStr', type=str, default='./dataset/validation/LR_1')
-parser.add_argument('--TestDataIndex', type=int, default=35)
-parser.add_argument('--apsDataStr', type=str, default='E:/zihao/Winston/data/nfs-evsr-events/aps')
-parser.add_argument('--apsTrainTimestampStr', type=str, default='./dataset/train/nfs_frame_timestamp')
-parser.add_argument('--apsValidationTimestampStr', type=str, default='./dataset/validation/nfs_frame_timestamp')
-parser.add_argument('--apsTestTimestampStr', type=str, default='./dataset/test/nfs_frame_timestamp')
+t/test/nfs_frame_timestamp')
 parser.add_argument('--max_train_dataset_size', type=int, default=5)
 parser.add_argument('--max_validation_dataset_size', type=int, default=3)
 parser.add_argument('--event_norm', action='store_true')
